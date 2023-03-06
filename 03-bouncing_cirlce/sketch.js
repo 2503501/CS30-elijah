88b8407eac5107dbbb1c05d937db12737425a798
2503501 <126823117+2503501@users.noreply.github.com>
2023-03-06 15:17:34 -0600

    ball stuff

---
+++ 03-bouncing_cirlce/sketch.js
@@ -0,0 +1,57 @@
+let shapes = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  displayShapes();
+  moveShapes();
+  bounceachother();
+}
+
+function moveShapes(){
+  for (let i=0; i<shapes.length; i++){
+    shapes[i].x += shapes[i].dx;
+    shapes[i].y += shapes[i].dy;
+    if (shapes[i].x < 0 + shapes[i].diameter/2  || shapes[i].x > windowWidth - shapes[i].diameter/2  ){
+      shapes[i].dx =shapes[i].dx * -1;
+    }
+    if (shapes[i].y < 0 + shapes[i].diameter/2  || shapes[i].y > windowHeight - shapes[i].diameter/2  ){
+      shapes[i].dy =shapes[i].dy * -1;
+    }
+  }
+}
+
+// function bounceachother(){
+//   for (let n=0; n<shapes.length; n++){
+//     for (let i=0; i<shapes.length; i++){
+//       if (shapes[n].x > shapes[i].x + shapes[i].diameter/2  && shapes[n].x > shapes[i].x - shapes[i].diameter/2 ){
+//         shapes[i].dy =shapes[i].dy * -1;
+//       }
+// }
+
+function mousePressed(){
+  createBall(mouseX, mouseY);
+}
+
+function displayShapes(){
+  for (let i=0; i < shapes.length; i++){
+    fill(shapes[i].thecolor);
+    circle(shapes[i].x,shapes[i].y, shapes[i].diameter);
+  }
+}
+
+
+function createBall(tempx, tempy){
+  let newball = {
+    x: tempx,
+    y: tempy,
+    dx: random(-5, 5),
+    dy: random(-5, 5),
+    diameter: random(25,100),
+    thecolor: color(random(255), random(255), random(255))
+  };
+  shapes.push(newball);
+}
