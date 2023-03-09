af38a30e3faf3245ac0a6d574ba54590f396cabf
2503501 <126823117+2503501@users.noreply.github.com>
2023-03-09 13:09:31 -0600

    e

--- 04-crazy_squares/sketch.js
+++ 04-crazy_squares/sketch.js
@@ -1,32 +1,41 @@
-// cubic disarray
+// Cubic Disarray Demo
+// March 7, 2023
 
 let boxes = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
   angleMode(DEGREES);
 
-//   for (let i = 0; i <15; i++){
-//   }
-// }
 
-for (let y = 0; y <height; y += 50){
-  for (let x = 0; x < width; x += 50){
-    
-    spawnBox(random(width), random(height), 50, 45);
+  for (let y = 0; y < height; y += 50) {
+    for (let x = 0; x < width; x += 50) {
+      spawnBox(x, y, 50, random(y*0.2));
+    }
   }
- }
+}
 
 function draw() {
   background(220);
 
-  for (let i = 0; i < boxes.length, i ++) {
+  //display all boxes
+  for (let i = 0; i < boxes.length; i++) {
     displayBox(boxes[i]);
-  
   }
+  let angle =+ 0.1;
 }
 
-function spawnBox(theX, theY, theSize, howRotated){
+function displayBox(myBox) {
+  push(); //saving the transformation matrix
+  rectMode(CENTER);
+  translate(myBox.x, myBox.y);
+  rotate(myBox.rotation);
+  square(0, 0, myBox.size);
+  pop(); //resetting the transformation matrix
+}
+
+function spawnBox(theX, theY, theSize, howRotated) {
   let someBox = {
     x: theX,
     y: theY,
@@ -35,12 +44,3 @@ function spawnBox(theX, theY, theSize, howRotated){
   };
   boxes.push(someBox);
 }
-
-
-function displayBox(myBox){
-  push(); // save the state
-  translate(myBox.x, myBox.y);
-  rotate(myBox.rotation);
-  square(0, 0, myBox.size);
-  pop(); //reset  the state
-}}
--- array-assignment/sketch.js
+++ array-assignment/sketch.js
@@ -1,15 +1,51 @@
-// Project Title
-// Your Name
-// Date
+// Array and Object Noatation Assignment
+// Elijah Zhao
+// March 8th 2023
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let boxes = [];
+let angle = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+for (let i = 0; i < width; i += 50){
+  spawnBox(i, height/2, 0);
+  // rect(i, height/2, 50, 200);
+}
+
+
 function draw() {
   background(220);
+  angle = angle + 0.1;
+
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].boxheight = map(sin(angle), -1, 1, 0, 100);
+    // spawnBox(i, height/2, map(sin(angle), -1, 1, 0, 100));
+  }
+
+  for (let i = 0; i < boxes.length; i++) {
+    displayBox(boxes[i]);
+  }
+}
+
+
+function spawnBox(theX, theY, heightsize) {
+  let someBox = {
+    x: theX,
+    y: theY,
+    boxheight: heightsize,
+  };
+  boxes.push(someBox);
+}
+
+function displayBox(myBox) {
+  push(); //saving the transformation matrix
+  rectMode(CENTER);
+  translate(myBox.x, myBox.y);
+  rect(0 + 25, 0, 50, myBox.boxheight);
+  pop(); //resetting the transformation matrix
 }
