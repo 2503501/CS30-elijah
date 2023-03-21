e1a047b05b3a0bdbff08ae0996fa4a012225a5a4
2503501 <126823117+2503501@users.noreply.github.com>
2023-03-21 14:48:33 -0600

    Update sketch.js

--- 07-grid-demo/sketch.js
+++ 07-grid-demo/sketch.js
@@ -5,10 +5,10 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let grid = [[0, 0, 1, 1], 
-            [1, 1, 0, 0], 
-            [0, 1, 0, 1], 
-            [1, 1, 1, 1]];
+let grid = [[0, 0, 0, 1], [1, 0, 0, 0], [0, 1, 0, 1], [1, 1, 1, 1]];
+
+let gridWidth = 4;
+let gridHeight = 4;
 
 
 function setup() {
@@ -17,9 +17,40 @@ function setup() {
 
 function draw() {
   background(220);
+  randomGrid();
+  displayGrid();
+}
+
+
+function KeyTyped(){
+  if (key === "r"){
+    randomGrid();
+  }
 }
 
 
 function displayGrid(){
-  for (let i =0; i < grid.length; )
+  for (let i =0; i < grid.length; i++){
+    for (let x =0; x < grid[0].length; x++){
+      if (grid[i][x] < 5){
+        fill("black");
+      }
+      else if (grid[i][x] >= 5){
+        fill("white");
+      }
+      rect(i * (width/grid.length), x * (height/grid[i].length), grid.length * (width/grid.length),  grid.length * (height/grid[i].length) );
+
+    }
+
+  }
+}
+
+function randomGrid(){
+  for (let i = 0; i < gridWidth; i ++){
+    grid.pop();
+  }
+  for (let i = 0; i < gridWidth; i ++){
+    grid.push([random(10), random(10), random(10),random(10)]);
+  }
+
 }
